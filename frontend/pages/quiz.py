3f1d5e4cf9ac87381d37f66f44442a233fc77ca1
Issy Smith <IssySmith@ibm.com>
2026-02-10 10:22:43 +0000

    trying to load questions into frontend

--- frontend/pages/quiz.py
+++ frontend/pages/quiz.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from pages.widgets import hide_toolbar
+from ...backend.question_loader import load_questions
 
 hide_toolbar()
 
 st.title("This is the quiz")
+
+questions = load_questions()    
 
